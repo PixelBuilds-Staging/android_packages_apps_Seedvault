9fbbf0ef32032cbff5407be4d109442c327fabaa
Torsten Grote <t@grobox.de>
2024-04-12 17:54:55 -0300

    Add the UI for the WebDAV plugin
    including ViewModel and StoragePluginManager logic

--- app/src/main/java/com/stevesoltys/seedvault/App.kt
+++ app/src/main/java/com/stevesoltys/seedvault/App.kt
@@ -20,6 +20,7 @@ import com.stevesoltys.seedvault.metadata.MetadataManager
 import com.stevesoltys.seedvault.metadata.metadataModule
 import com.stevesoltys.seedvault.plugins.StoragePluginManager
 import com.stevesoltys.seedvault.plugins.saf.storagePluginModuleSaf
+import com.stevesoltys.seedvault.plugins.webdav.storagePluginModuleWebDav
 import com.stevesoltys.seedvault.restore.RestoreViewModel
 import com.stevesoltys.seedvault.restore.install.installModule
 import com.stevesoltys.seedvault.settings.AppListRetriever
@@ -54,7 +55,7 @@ open class App : Application() {
     private val appModule = module {
         single { SettingsManager(this@App) }
         single { BackupNotificationManager(this@App) }
-        single { StoragePluginManager(this@App, get(), get()) }
+        single { StoragePluginManager(this@App, get(), get(), get()) }
         single { Clock() }
         factory<IBackupManager> { IBackupManager.Stub.asInterface(getService(BACKUP_SERVICE)) }
         factory { AppListRetriever(this@App, get(), get(), get()) }
@@ -73,9 +74,6 @@ open class App : Application() {
             )
         }
         viewModel { RecoveryCodeViewModel(this@App, get(), get(), get(), get(), get(), get()) }
-        viewModel { BackupStorageViewModel(this@App, get(), get(), get(), get(), get(), get()) }
-        viewModel { RestoreStorageViewModel(this@App, get(), get(), get()) }
-        viewModel { RestoreViewModel(this@App, get(), get(), get(), get(), get(), get(), get()) }
         viewModel {
             BackupStorageViewModel(
                 app = this@App,
@@ -83,11 +81,12 @@ open class App : Application() {
                 backupInitializer = get(),
                 storageBackup = get(),
                 safHandler = get(),
+                webDavHandler = get(),
                 settingsManager = get(),
                 storagePluginManager = get(),
             )
         }
-        viewModel { RestoreStorageViewModel(this@App, get(), get(), get()) }
+        viewModel { RestoreStorageViewModel(this@App, get(), get(), get(), get()) }
         viewModel { RestoreViewModel(this@App, get(), get(), get(), get(), get(), get(), get()) }
         viewModel { FileSelectionViewModel(this@App, get()) }
     }
@@ -126,7 +125,8 @@ open class App : Application() {
         cryptoModule,
         headerModule,
         metadataModule,
-        storagePluginModuleSaf, // storage plugin
+        storagePluginModuleSaf,
+        storagePluginModuleWebDav,
         backupModule,
         restoreModule,
         installModule,
