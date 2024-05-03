2e8610510a56fd35e011b7a93feb449b00c1ff52
Torsten Grote <t@grobox.de>
2024-05-03 15:57:01 -0300

    Replace field naming _foo with mFoo

--- app/src/main/java/com/stevesoltys/seedvault/settings/SettingsViewModel.kt
+++ app/src/main/java/com/stevesoltys/seedvault/settings/SettingsViewModel.kt
@@ -100,11 +100,11 @@ internal class SettingsViewModel(
     private val mAppEditMode = MutableLiveData<Boolean>()
     internal val appEditMode: LiveData<Boolean> = mAppEditMode
 
-    private val _filesSummary = MutableLiveData<String>()
-    internal val filesSummary: LiveData<String> = _filesSummary
+    private val mFilesSummary = MutableLiveData<String>()
+    internal val filesSummary: LiveData<String> = mFilesSummary
 
-    private val _initEvent = MutableLiveEvent<Boolean>()
-    val initEvent: LiveEvent<Boolean> = _initEvent
+    private val mInitEvent = MutableLiveEvent<Boolean>()
+    val initEvent: LiveEvent<Boolean> = mInitEvent
 
     private val storageObserver = object : ContentObserver(null) {
         override fun onChange(selfChange: Boolean, uris: MutableCollection<Uri>, flags: Int) {
@@ -258,7 +258,7 @@ internal class SettingsViewModel(
     @UiThread
     fun loadFilesSummary() = viewModelScope.launch {
         val uriSummary = storageBackup.getUriSummaryString()
-        _filesSummary.value = uriSummary.ifEmpty {
+        mFilesSummary.value = uriSummary.ifEmpty {
             app.getString(R.string.settings_backup_files_summary)
         }
     }
@@ -274,10 +274,10 @@ internal class SettingsViewModel(
                 }
                 viewModelScope.launch(Dispatchers.IO) {
                     backupInitializer.initialize(onError) {
-                        _initEvent.postEvent(false)
+                        mInitEvent.postEvent(false)
                         scheduleAppBackup()
                     }
-                    _initEvent.postEvent(true)
+                    mInitEvent.postEvent(true)
                 }
             }
             // enable call log backups for existing installs (added end of 2020)
