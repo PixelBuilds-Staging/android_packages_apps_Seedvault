b48b82f4efff5246fb1e4ea23517de84ff8a5c9b
Torsten Grote <t@grobox.de>
2024-06-18 11:15:50 -0300

    Disable framework scheduling when scheduling backup, because another transport may have re-enabled it

--- app/src/main/java/com/stevesoltys/seedvault/ui/storage/BackupStorageViewModel.kt
+++ app/src/main/java/com/stevesoltys/seedvault/ui/storage/BackupStorageViewModel.kt
@@ -8,6 +8,7 @@ package com.stevesoltys.seedvault.ui.storage
 import android.app.Application
 import android.app.backup.IBackupManager
 import android.app.job.JobInfo
+import android.os.UserHandle
 import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.stevesoltys.seedvault.R
@@ -99,6 +100,8 @@ internal class BackupStorageViewModel(
 
     private fun scheduleBackupWorkers() {
         val storage = storagePluginManager.storageProperties ?: error("no storage available")
+        // disable framework scheduling, because another transport may have enabled it
+        backupManager.setFrameworkSchedulingEnabledForUser(UserHandle.myUserId(), false)
         if (!storage.isUsb) {
             if (backupManager.isBackupEnabled) AppBackupWorker.schedule(app)
             if (settingsManager.isStorageBackupEnabled()) BackupJobService.scheduleJob(
