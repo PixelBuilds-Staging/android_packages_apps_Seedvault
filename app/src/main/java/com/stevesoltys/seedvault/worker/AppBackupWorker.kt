e0a232bc80326845aab2301a4587d4a634f6259c
Torsten Grote <t@grobox.de>
2024-09-17 10:21:13 -0300

    Move new classes to repo package
    as they all relate to interacting with the backup repository

--- app/src/main/java/com/stevesoltys/seedvault/worker/AppBackupWorker.kt
+++ app/src/main/java/com/stevesoltys/seedvault/worker/AppBackupWorker.kt
@@ -24,7 +24,7 @@ import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.stevesoltys.seedvault.R
 import com.stevesoltys.seedvault.backend.BackendManager
-import com.stevesoltys.seedvault.transport.backup.AppBackupManager
+import com.stevesoltys.seedvault.repo.AppBackupManager
 import com.stevesoltys.seedvault.ui.notification.BackupNotificationManager
 import com.stevesoltys.seedvault.ui.notification.NOTIFICATION_ID_OBSERVER
 import org.koin.core.component.KoinComponent
