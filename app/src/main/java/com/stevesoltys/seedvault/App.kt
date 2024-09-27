b406d148038c8a2da9e6f0251ef3624e708146ac
Torsten Grote <t@grobox.de>
2024-09-27 10:30:43 -0300

    do scheduling migration before strict mode
    otherwise all LoggingFactory ClassLoader lookups that cause disk reads are logged when koin initializes classes

--- app/src/main/java/com/stevesoltys/seedvault/App.kt
+++ app/src/main/java/com/stevesoltys/seedvault/App.kt
@@ -113,6 +113,7 @@ open class App : Application() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
         startKoin()
+        if (!isTest) migrateToOwnScheduling()
         if (isDebugBuild()) {
             StrictMode.setThreadPolicy(
                 StrictMode.ThreadPolicy.Builder()
@@ -128,7 +129,6 @@ open class App : Application() {
                     .build()
             )
         }
-        if (!isTest) migrateToOwnScheduling()
     }
 
     protected open fun startKoin() = startKoin {
