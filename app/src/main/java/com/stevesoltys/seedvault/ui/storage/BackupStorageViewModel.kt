ac74fd7a1d6e75e7310a52328b3179052f509ea5
Torsten Grote <t@grobox.de>
2025-01-07 10:35:44 -0300

    log when file backup gets scheduled and add TODOs

--- app/src/main/java/com/stevesoltys/seedvault/ui/storage/BackupStorageViewModel.kt
+++ app/src/main/java/com/stevesoltys/seedvault/ui/storage/BackupStorageViewModel.kt
@@ -122,6 +122,8 @@ internal class BackupStorageViewModel(
             if (backupManager.isBackupEnabled) {
                 AppBackupWorker.schedule(app)
             }
+            // FIXME this runs a backup right away (if constraints fulfilled)
+            //  and JobScheduler doesn't offer initial delay
             if (settingsManager.isStorageBackupEnabled()) BackupJobService.scheduleJob(
                 context = app,
                 jobServiceClass = StorageBackupJobService::class.java,
